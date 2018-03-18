2c4785520259ea817b1dee9817562bcf7e5e7f6e
Matt Petters <matt@petters.io>
2018-03-18 17:02:36 -0400

    Working on login

--- login.php
+++ login.php
@@ -2,13 +2,33 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>Logging in...</title>
 </head>
 <body>
 <? php 
     $username = $_POST["username"]
     $pwd $_POST["pwd"];
-    $sql = "select * from user where username='$username'";
+    $validCrentials = $username != '' && $pwd != '';
+
+    if (!validCredentials) {
+        header('Location: home.html');
+         exit();
+    } else {
+        //check database to see if user exists
+        echo $username;
+        echo $pwd;
+        $sql = "select * from user where username='$username'";
+        //check to make sure passwords match
+        
+    
+    }
+
+
+    /* 1. See both username and password are entered. If not, redirect to “home.html” */
+/* 2. If password does not match, print “passwords did not match” and give a link to */
+/* “home.html” */
+/* 3. If both username and password match, start a session and create a session variable set for */
+/* username. Then, redirect to “user_home.php”. */
 ?>
 </body>
 </html>
